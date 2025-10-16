7430441a6aea5bc8aed7d7a8cb8271650cfb9cfb
Isabella Machado <91352337+isabellammachado@users.noreply.github.com>
2025-10-15 22:25:03 -0300

    Update Livro.java
    add column

--- biblioteca/src/main/java/org/serratec/biblioteca/domain/Livro.java
+++ biblioteca/src/main/java/org/serratec/biblioteca/domain/Livro.java
@@ -1,5 +1,6 @@
 package org.serratec.biblioteca.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -7,6 +8,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 
 @Entity
@@ -17,11 +19,14 @@ public class Livro {
 		@GeneratedValue(strategy= GenerationType.IDENTITY)
 		private Long id;
 		
+		@Positive
 		@NotBlank(message = "O título não pode ultrapassar 40 caracteres")
 		@Size(max=40)
+		@Column(length = 40 , nullable =false)
 		private String titulo;
 		
-		@NotNull
+		@Positive
+		@NotNull(message = "Insira um valor válido")
 		private Integer qntPaginas;
 
 		@Embedded
